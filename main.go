6ae109ec27163de3394f595fe0ba29500a1ebe2f
smallsonxdd <jan.lhotak123@gmail.com>
2025-01-13 16:32:23 +0100

    1-11

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 	server.ListenAndServe()
 
 }
